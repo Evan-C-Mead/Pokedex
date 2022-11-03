fe77c0e0ad0f3f14688775a6c68707b4427b5012
Evan-C-Mead <evan.c.mead@gmail.com>
2022-11-03 01:40:51 -0500

    fixed collapsable navbar issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,18 @@ import React from "react";
 
 export default function Navbar() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle"
-                    aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
+                    aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                 <span><img src="/media/pokemon/poke-ball.png" alt="poke_ball" width="auto" height="25"/></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarToggle">
-                <ul class="navbar-nav mr-auto mt-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.html">Poké-Page</a>
+            <div className="collapse navbar-collapse" id="navbarToggler">
+                <ul className="navbar-nav mr-auto mt-0">
+                    <li className="nav-item">
+                        <a className="nav-link" href="index.html">Poké-Page</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://www.pokemon.com/us/" target="_blank">Pokémon</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="https://www.pokemon.com/us/" target="_blank">Pokémon</a>
                     </li>
                 </ul>
             </div>
